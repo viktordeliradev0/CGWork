e9b8f980d06cf466934ca19627a63d9ab1671428
viktordeliradev0 <108656783+viktordeliradev0@users.noreply.github.com>
2026-09-14 17:08:33 +0300

    Update before run

--- CGWork/Program.cs
+++ CGWork/Program.cs
@@ -15,14 +15,19 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "CGWork API v1");
+    c.RoutePrefix = "swagger"; // Достъпен на /swagger
+}); 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
 app.Run();
